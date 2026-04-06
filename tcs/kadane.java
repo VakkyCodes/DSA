7445ecd44d9c8cd98c16a7c597af8dcbfb887ec2
VakkyCodes <vakratundkulkarni88@gmail.com>
2026-04-06 21:11:25 +0530

    done with the new file

---
+++ tcs/kadane.java
@@ -0,0 +1,17 @@
+public class kadane {
+    public static void main(String[] args) {
+        int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int num : arr) {
+            currentSum += num;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+}
--- tcs/test.java
+++ tcs/test.java
@@ -1,21 +1,12 @@
 public class test {
-    public static void test1(){
-        int arr[]={10,20,4,45};
-        int largest=Integer.MIN_VALUE;
-        int secondlargest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>=largest){
-                secondlargest=largest;
-                largest=arr[i];     
-               }
-               else if(arr[i]>secondlargest && arr[i]!=largest){
-                secondlargest=arr[i];
-               }
-        }
-        System.out.println("this is the largest"+largest+"this is the second largest"+secondlargest);
+    public static void test(){
+        int arr[]={0, 1, 0, 3, 1, 2};
+        int index=0;
+       
+
     }
     
     public static void main(String[] args) {
-        test1();
+        test();
     }
 }
