91619190e838d67e8aeeda9c3777d374c75f5061
VakkyCodes <vakratundkulkarni88@gmail.com>
2026-04-06 21:41:30 +0530

    done with the new file

--- tcs/test.java
+++ tcs/test.java
@@ -1,9 +1,9 @@
 public class test {
     public static void test(){
-        int arr[]={0, 1, 0, 3, 1, 2};
-        int index=0;
+        int index=2;
+        int ch = (char)('A'+2);
        
-
+System.out.println(ch);
     }
     
     public static void main(String[] args) {
