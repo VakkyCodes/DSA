aae35a4dd70609819a2a7d5f0d26e543510e8153
VakkyCodes <vakratundkulkarni88@gmail.com>
2026-04-11 11:57:11 +0530

    map are need to be conquered

--- tcs/neew.java
+++ tcs/neew.java
@@ -1,54 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class neew{
-    public static void ana(){
-      Scanner sc=new Scanner(System.in);
-      System.err.println("enter the size of array");
-      int n=sc.nextInt();
-     int arr[]=new int[n];
-     for(int i=0;i<n;i++){
-      arr[i]=sc.nextInt();
-     } 
-     if(arr.length==0){
-      return;
-     }
-     for(int i=0;i<n;i++){
-      int count=1;
-      for(int j=i+1;j<n;j++){
-        if(arr[i]==arr[j]){
-          count ++;
-        }
-      if(count%2!=0){
-      System.out.println("this is the imposter"+arr[j]);
-      return;
-     }
-     } 
-     
-     } 
-}
-public static void opt(){
-  int arr[]={0,0};
-  if(arr.length==2){
-  System.out.println("array is just of 2 element");
-  return;
-}
-  HashMap<Integer,Integer>map=new HashMap<>();
-  for(int i=0;i<arr.length;i++){
-    map.put(arr[i], map.getOrDefault(arr[i],0)+1);
-  }
-  for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-    if (entry.getValue() % 2 != 0) {
-        System.out.println("this is the imposter " + entry.getKey());
-        
+    public static void prime(){
+   int n=3;
+    for(int i=2;i<Math.sqrt(n);i++){
+      if(n%i==0){
+        System.out.println("it is not prime");
+      }
+      else System.out.println("it is prime");
     }
+
+      
 }
 
-  }
 
     public static void main(String[]args){
-      opt();
+         int arr[]={1,2,1,3,2,5,3,7,5,11};
+      prime();
 
     }
 }
