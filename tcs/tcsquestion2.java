082762fc1b8e4b5507157b07265ea360efc60fda
VakkyCodes <vakratundkulkarni88@gmail.com>
2026-04-23 21:49:10 +0530

    this is what it is

--- tcs/tcsquestion2.java
+++ tcs/tcsquestion2.java
@@ -1,9 +1,10 @@
 import java.util.*;
-public class test {
-public static boolean check(int mat[][]){
-for(int i=0;i<2;i++){
-for(int j=0;j<2;j++){
-if(mat[i][j]!=mat[i+1][j+1] && i!=1 && j!=1){
+public class tcsquestion2 {
+public static boolean check(int mat[][],int n){
+    
+for(int i=0;i<n-1;i++){
+for(int j=0;j<n-1;j++){
+if(mat[i][j]!=mat[i+1][j+1]){
 return false;
 }
 }
@@ -12,21 +13,22 @@ return true;
 }
     public static void main(String[]args){
        Scanner sc=new Scanner(System.in);
-int mat[][]=new int[2][2];
+       int n=4;
+int mat[][]=new int[n][n];
 System.out.println("enter the number");
-for(int i=0;i<2;i++){
-for(int j=0;j<2;j++){
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
 mat[i][j]=sc.nextInt();
 }
 }
 int sum=0;
-if(check(mat)){
-for(int i=0;i<2;i++){
+if(check(mat, n)){
+for(int i=0;i<n;i++){
 sum+=mat[0][i];
 }
 }
 else{
-for(int i=0;i<2;i++){
+for(int i=0;i<n;i++){
 sum+=2*mat[0][i];
 }
 }
